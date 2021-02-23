58a93ff7e64bfd3f80c5efe10e5f6584a68f081b
Siddharthan <sidtypist@gmail.com>
2021-02-23 11:27:52 +0530

    Additional Pages for Project Initialization

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "./Homepage/Navbar";
+
+function App() {
+  return <Navbar />;
+}
+
+export default App;
---
+++ src/components/Homepage/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <h1>Hi Bro</h1>;
+}
+
+export default Navbar;
